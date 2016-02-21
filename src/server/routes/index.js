35df7867fc3d54914757b37e39886638971ebd2c
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-02-21 10:30:39 -0700

    Added layout for about me page

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -5,4 +5,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/about', function(req, res, next) {
+    res.render('about', {title: 'Mike', about: ['likes cats more than dogs', 'races cars', 'loves building things']})
+});
+
 module.exports = router;
